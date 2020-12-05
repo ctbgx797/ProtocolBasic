a9b2727754428bb8d43f43eb77b2a80bb92a6254
Yoshiki <villager2525@gmail.com>
2020-12-05 15:20:36 +0900

    first commit

---
+++ ProtocolBasic/NextViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NextViewController.swift
+//  ProtocolBasic
+//
+//  Created by 西谷恭紀 on 2020/12/05.
+//
+
+import UIKit
+
+protocol CatchProtocolDelegate {
+    //規則を決める
+    func catchData(count:Int)
+}
+
+class NextViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    
+    var count:Int = 0
+    
+    var delegate:CatchProtocolDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func plusAction(_ sender: Any) {
+        
+        count = count+1
+        label.text = String(count)
+        
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        
+    }
+}
